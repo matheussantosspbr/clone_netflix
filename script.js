f51fbd081beef340884aba13fa7249945f1c93ab
Matheus Santos <87808357+matheussantosspbr@users.noreply.github.com>
2022-02-18 16:01:15 -0300

    #1

---
+++ script.js
@@ -0,0 +1,14 @@
+function clicar() {
+  var click = document.querySelector('#click')
+
+  click.innerHTML = `
+  <p>
+                  As informações recolhidas pelo Google reCAPTCHA estão sujeitas
+                  à <span>Política de Privacidade</span> e
+                  <span>Termos de Uso</span>, e são usadas para oferecer, manter
+                  e melhorar o serviço reCAPTCHA e por questões de segurança
+                  (não são usadas para exibir anúncios personalizados pelo
+                  Google).
+                </p>
+  `
+}
